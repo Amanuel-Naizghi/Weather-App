6a1d383af99f0cd274dc33b291f3af9f613417af
Amanuel-Naizghi <naizghiaman@gmail.com>
2024-09-17 20:29:07 +0300

    The code has been formatted using eslint and prettier for a better readability

--- src/fetch.js
+++ src/fetch.js
@@ -1,72 +1,71 @@
-import {importDataToDom} from './DOM.js';
+import { importDataToDom } from './DOM.js';
 
-async function fetchData(city){
-    try{
-        let loadingDialog=document.querySelector('#loading-dialog');
-        loadingDialog.showModal();
-        let response=await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(city)}?unitGroup=metric&key=VFS2WWLFWATZWDP439374ZTZY&contentType=json`,{mode:'cors'});
-        let data=await response.json();
-        importDataToDom(data);
-        loadingDialog.close();
-    }
-    catch(err){
-        let errorDialog=document.querySelector('#error-dialog');
-        let errorText=document.querySelector('#error-dialog>p');
-        errorText.textContent=`${err}`;
-        errorDialog.showModal();
-        setTimeout(()=>{
-            errorDialog.close();
-        },5000);
-        fetchData('Asmara');
-    }
+async function fetchData(city) {
+  try {
+    const loadingDialog = document.querySelector('#loading-dialog');
+    loadingDialog.showModal();
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${encodeURIComponent(city)}?unitGroup=metric&key=VFS2WWLFWATZWDP439374ZTZY&contentType=json`,
+      { mode: 'cors' },
+    );
+    const data = await response.json();
+    importDataToDom(data);
+    loadingDialog.close();
+  } catch (err) {
+    const errorDialog = document.querySelector('#error-dialog');
+    const errorText = document.querySelector('#error-dialog>p');
+    errorText.textContent = `${err}`;
+    errorDialog.showModal();
+    setTimeout(() => {
+      errorDialog.close();
+    }, 5000);
+    fetchData('Asmara');
+  }
 }
 
-function getCurrentDayInfo(dateInfo){
-    let dayData=new Date(dateInfo);
-    let day=dayData.getDate();
-    let month=dayData.getMonth()+1;
-    let year=dayData.getFullYear();
+function getCurrentDayInfo(dateInfo) {
+  const dayData = new Date(dateInfo);
+  const day = dayData.getDate();
+  const month = dayData.getMonth() + 1;
+  const year = dayData.getFullYear();
 
-    let getActualDayMonthYear=()=>{
-        const date=new Date(year,month-1,day);
-        const options={year:'numeric',month:'long',day:'numeric'}
-        return date.toLocaleString('en-US',options);
-    };
+  const getActualDayMonthYear = () => {
+    const date = new Date(year, month - 1, day);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleString('en-US', options);
+  };
 
-    let getActualDayMonth=()=>{
-        const date=new Date(year,month-1,day);
-        const options={year:'numeric',month:'long',day:'numeric'}
-        return date.toLocaleString('en-US',options);
-    };
+  const getActualDayMonth = () => {
+    const date = new Date(year, month - 1, day);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleString('en-US', options);
+  };
 
-    let getDayName=()=>{
-        const date=new Date(dateInfo);
-        return date.toLocaleString('en-US',{weekday:'long'});
-    };
+  const getDayName = () => {
+    const date = new Date(dateInfo);
+    return date.toLocaleString('en-US', { weekday: 'long' });
+  };
 
-    return{getDayName,getActualDayMonthYear,getActualDayMonth};
+  return { getDayName, getActualDayMonthYear, getActualDayMonth };
 }
 
-function timeFormatter(time){
-    let myTime=time.slice(0,5);
-    let myHour=myTime.slice(0,2);
-    let myMinute=myTime.slice(3,5);
-    let timeIndicator;
+function timeFormatter(time) {
+  const myTime = time.slice(0, 5);
+  let myHour = myTime.slice(0, 2);
+  const myMinute = myTime.slice(3, 5);
+  let timeIndicator;
 
-    if(myHour>12){
-        myHour-=12;
-        timeIndicator="Pm";
-    }
-    else if(myHour==='12'){
-        timeIndicator='Pm';
-    }
-    else{
-        timeIndicator='Am';
-    }
+  if (myHour > 12) {
+    myHour -= 12;
+    timeIndicator = 'Pm';
+  } else if (myHour === '12') {
+    timeIndicator = 'Pm';
+  } else {
+    timeIndicator = 'Am';
+  }
 
-    let myNewTime=`${myHour}:${myMinute}`;
-    return {myNewTime,timeIndicator};
+  const myNewTime = `${myHour}:${myMinute}`;
+  return { myNewTime, timeIndicator };
 }
 
-
-export {fetchData,getCurrentDayInfo,timeFormatter};
+export { fetchData, getCurrentDayInfo, timeFormatter };
